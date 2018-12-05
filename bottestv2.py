7f608f1351fac9131b9ebb40560a3712165e0eca
NikNikYT <45605688+NikNikYT@users.noreply.github.com>
2018-12-05 12:14:57 +0100

    Update bottestv2.py

--- bottestv2.py
+++ bottestv2.py
@@ -35,7 +35,7 @@ async def on_message(message):
         await client.send_message(message.channel,'its try me bitch <@%s>'  %(message.author.id))
     if ('fuck dig bitch') in message.content:
        await client.delete_message(message)
-    if message.content == 'hvad er niknik's fav sang':
+    if message.content == '+fav sang':
         await client.send_message(message.channel,'https://www.youtube.com/watch?v=-yHoa4MR0W4 her')
     if message.content.startswith('+8ball'):
         randomlist = ["du er heldig resten af dagen","du kommer til at dø ung","vidste du vis du spiser for meget kage så bliver du fed?","10 minuter mere mor.","prøv senere","jeg har en mega hovedet pine så gå plz","shaun mendes er grim XD","måske","hvem ved","sikkert","er ikke sikker"]
