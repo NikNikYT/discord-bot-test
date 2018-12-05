a546a2fecaf779ed8c1ef63ec9da15977096d051
NikNikYT <45605688+NikNikYT@users.noreply.github.com>
2018-12-05 17:23:56 +0100

    Update bottestv2.py

--- bottestv2.py
+++ bottestv2.py
@@ -5,39 +5,54 @@ import asyncio
 import time
 import os
 import random
+import youtube_dl
 from discord import Game
 
+players = {}
 
-Client = discord.client
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix = '+')
 Clientdiscord = discord.Client()
 
 
 @client.event
-async def on_member_join(member):
-    print('Recognised that a member called ' + member.name + ' joined')
-    await client.send_message(member, 'hej og velkommen')
-    print('Sent message to ' + member.name)
 async def on_ready():
-    await client.change_presence(game=Game(name='ost'))
-    print('Ready, Freddy') 
+    await client.change_presence(game=Game(name='you sleep.', type = 3))
+    print('Bot Online.')
 
 
-@client.event
-async def on_message(message):
-    if message.content == '+commands':
-        await client.send_message(message.channel,'all the commands havent yeet been added')
-    if message.content == '+imgboi':
-        em = discord.Embed(description='where is my $40 bill XD')
-        em.set_image(url='https://i.imgflip.com/2o1kfo.jpg')
-        await client.send_message(message.channel, embed=em)
-    if message.content.startswith('+pingmebitch'):
-        await client.send_message(message.channel,'its try me bitch <@%s>'  %(message.author.id))
-    if ('fuck dig bitch') in message.content:
-       await client.delete_message(message)
-    if message.content == '+fav sang':
-        await client.send_message(message.channel,'https://www.youtube.com/watch?v=-yHoa4MR0W4 her')
-    if message.content.startswith('+8ball'):
-        randomlist = ["du er heldig resten af dagen","du kommer til at dø ung","vidste du vis du spiser for meget kage så bliver du fed?","10 minuter mere mor.","prøv senere","jeg har en mega hovedet pine så gå plz","shaun mendes er grim XD","måske","hvem ved","sikkert","er ikke sikker"]
-        await client.send_message(message.channel,(random.choice(randomlist)))
+
+@client.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
+
+@client.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
+
+@client.command(pass_context=True)
+async def play(ctx, url):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url)
+    players[server.id] = player
+    player.start()
+
+@client.command(pass_context=True)
+async def pause(ctx):
+        id = ctx.message.server.id
+        players[id].pause()
+
+@client.command(pass_context=True)
+async def stop(ctx):
+    id = ctx.message.server.id
+    players[id].stop()
+
+@client.command(pass_context=True)
+async def resume(ctx):
+    id = ctx.message.server.id
+    players[id].resume()
+
 client.run(os.getenv('TOKEN'))
